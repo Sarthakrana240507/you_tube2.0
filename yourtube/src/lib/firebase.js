e02dd6ea9343d95a6be3d9ae613baad42cca7d54
Sarthak Rana <prpoojarana79@gmail.com>
2026-01-21 22:18:20 +0530

    Fix: Update Firebase config with measurementId and corrected API Key

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -6,12 +6,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD3anC_HLNYsqTqIVJ0-VtsrlOlzQWGd6U",
+  apiKey: "AIzaSyD3anC_HLNYsqTqIVJO-VtsrlOlzQWGd6U",
   authDomain: "yourtube-f5eb8.firebaseapp.com",
   projectId: "yourtube-f5eb8",
   storageBucket: "yourtube-f5eb8.firebasestorage.app",
   messagingSenderId: "248406226951",
   appId: "1:248406226951:web:48a9fa96df41784d27d2a2",
+  measurementId: "G-EWFH83FHLJ"
 };
 
 // Initialize Firebase
